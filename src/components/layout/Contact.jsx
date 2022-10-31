46dbcfaf4fd4264272a1a229611048aac4b90972
kde66034 <kde66034@gmail.com>
2022-10-31 14:15:20 +0900

    first commit

---
+++ src/components/layout/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section className='cont__contact'>cont__contact</section>
+  )
+}
+
+export default Contact;
---
+++ src/components/layout/Contents.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Contents = ({children}) => {
+  return (
+    <main id="main">{children}</main>
+  )
+}
+
+export default Contents;
