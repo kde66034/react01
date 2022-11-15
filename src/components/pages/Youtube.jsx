cdfef8e410df2fcd7540f138181082aebe8f14b4
kde66034 <kde66034@gmail.com>
2022-11-15 19:55:45 +0900

    221115

--- src/components/pages/Youtube.jsx
+++ src/components/pages/Youtube.jsx
@@ -9,13 +9,14 @@ import YoutubeSearch from '../include/YoutubeSearch';
 import YoutubeCont from '../include/YoutubeCont';
 import Contact from '../layout/Contact';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 // tmp
-import dummy from "../../utils/dummy.json";
+// import dummy from "../../utils/dummy.json";
 
 export const Youtube = () => {
-  const [youtubes, setYoutubes] = useState(dummy.items);
-  const [randomVideos, setrandomVideos] = useState(dummy.items);
+  const [youtubes, setYoutubes] = useState([]);
+  const [randomVideos, setrandomVideos] = useState([]);
   const search = async (query) => {
     await fetch(
       `https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyAmOyKrz9aq1HX5kEwKEUGNmAvJ11pAkwo&part=snippet&q=${query}&maxResults=30&type=video`
@@ -24,6 +25,32 @@ export const Youtube = () => {
       .then((result) => setYoutubes(result.items))
       .catch((error) => console.log("error", error));
   };
+
+  useEffect(() => {
+    async function fetchYoutube() {
+      await fetch(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&q=webstoryboy&key=AIzaSyAmOyKrz9aq1HX5kEwKEUGNmAvJ11pAkwo&maxResults=20&type=video"
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setYoutubes(result.items);
+        })
+        .catch((error) => console.log("error", error));
+    }
+
+    async function fetchRandomYoutube() {
+      await fetch(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAmOyKrz9aq1HX5kEwKEUGNmAvJ11pAkwo&maxResults=20&type=video"
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setrandomVideos(result.items);
+        })
+        .catch((error) => console.log("error", error));
+    }
+    fetchYoutube();
+    fetchRandomYoutube();
+  }, []);
 
   return (
     <>
